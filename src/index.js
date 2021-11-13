540d928bcdcb2b2410d949edda41da4daff744c9
Kit <ksaisaard@outlook.com>
2021-11-13 19:10:18 +1030

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import { render } from "react-dom";
 import {
-  BrowserRouter,
+  HashRouter,
   Routes,
   Route
 } from "react-router-dom";
 import './css/index.css';
+import ReactDOM from 'react-dom'
 import Home from './components/Home';
 import NotFound from './components/NotFound';
 import REC from './components/Retail-Earnings-Calculator';
 
-const rootElement = document.getElementById("root");
-render(
-  <BrowserRouter basename='/website'>
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="retail-earnings-calculator" element={<REC/>}/>
-      <Route path="rec" element={<REC/>}/>
-      <Route path="*" element={<NotFound/>}/>
-    </Routes>
-  </BrowserRouter>,
-  rootElement
+ReactDOM.render(
+  <React.StrictMode>
+    <HashRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="retail-earnings-calculator" element={<REC/>}/>
+        <Route exact path="rec" element={<REC/>}/>
+        <Route exact path="*" element={<NotFound/>}/>
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
