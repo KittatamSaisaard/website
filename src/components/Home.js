cbae181bcdb749576a66191373388a0eccb6e229
Kit <ksaisaard@outlook.com>
2021-11-16 01:02:44 +1030

    Changed Typewriter library and added tsParticles

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,29 +2,95 @@ import React from 'react';
 import profile_logo from '../images/profile_pic.png';
 import linkedIn_logo from '../images/linkedIn_logo.png';
 import '../css/Home.css';
-import Typical from 'react-typical'
+import TypeIt from "typeit-react";
+import Particles from "react-tsparticles";
+
 
 function Home() {
-
-  const typical_wait_time = 1500;
-
   return (
     <div className="Home">
+    <Particles className="particles"
+        params={{
+          "background": {
+            "color": {
+              "value": "#f2f2f2"
+            },
+            "position": "50% 50%",
+            "repeat": "no-repeat",
+            "size": "cover"
+          },
+          "fullScreen": {
+            "zIndex": 1
+          },
+          "particles": {
+            "color": {
+              "value": "#4ca7d4"
+            },
+            "links": {
+              "distance": 150,
+              "enable": true,
+              "color": {
+                "value": "#e6e6e6"
+              }
+            },
+            "move": {
+              "enable": true
+            },
+            "size": {
+              "value": 2
+            }
+          },
+          "interactivity": {
+            "events": {
+              // "onClick": {
+              //   "enable": true,
+              //   "mode": "push"
+              // },
+              "onHover": {
+                "enable": true,
+                "mode": "grab",
+                "parallax": {
+                  "enable": true,
+                  "force": 60
+                }
+              }
+            },
+            "modes": {
+              "bubble": {
+                "distance": 400,
+                "duration": 2,
+                "opacity": 0.8,
+                "size": 40
+              },
+              "grab": {
+                "distance": 400
+              }
+            }
+          }
+        }} />
       <header className="Home-header">
-        <Typical className="Typical"
-          steps={[
-            'Hi, I\'m Kit', typical_wait_time, 
-            'Hi, I\'m Kit, a Computer Science Student 💻', typical_wait_time, 
-            'Hi, I\'m Kit, a Computer Science Student, at The University of Adelaide 🏫', typical_wait_time, 
-            'Hi, I\'m Kit, a Computer Science Student, Majoring in Artifical Intelligence 🤖', typical_wait_time, 
-            'Hi, I\'m Kit, a Software Developer 👨‍💻', typical_wait_time, 
-            'Hi, I\'m Kit, a Volunteer 🙋‍♂️', typical_wait_time,
-            'Hi, I\'m Kit, a Nightfill Assistant 🛒', typical_wait_time,
-            'Hi, I\'m Kit, a Speedcuber 🏆', typical_wait_time,
-          ]}
-          loop={Infinity}
-          wrapper="p"
-        />
+        <div className="Typeit">
+          <TypeIt className="Typical" options={{cursorChar: "|", loop: true}}
+            getBeforeInit={(instance) => {
+              instance
+                .type("Hi, I'm <b>Kit</b>.", {speed: 115})
+                .pause(2000).delete(1).pause(200).type(", a <b>Computer Science</b> Student 💻", {speed: 50})
+                .pause(1500).delete(2).pause(200)
+                .type(", Majoring in <b>Artifical Intelligence</b> 🤖", {speed: 40})
+                .pause(2500).delete(1, {deleteSpeed: 20}).pause(200)
+                .type("at The <b>University of Adelaide</b> 🏫", {speed: 40})
+                .pause(2500).delete(95, {deleteSpeed: 15}).pause(200)
+                .type(" a <b>Software Developer</b> 💻", {speed: 40})
+                .pause(2500).delete(23, {deleteSpeed: 15}).pause(200)
+                .type(" a <b>Volunteer</b> ✋", {speed: 40})
+                .pause(2500).delete(14, {deleteSpeed: 15}).pause(200)
+                .type(" a <b>Night Fill Assistant</b> 🛒", {speed: 40})
+                .pause(2500).delete(null, {deleteSpeed: 15}).pause(400)
+
+              return instance;
+            }}
+          />
+        </div>
         <img src={profile_logo} className="Profile-logo" alt="logo" />
       </header>
       <hr/>
