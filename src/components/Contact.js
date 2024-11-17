21489306122d03c93fee0c29173ac11b33cea832
Kit <50354903+ksaisaard@users.noreply.github.com>
2024-11-17 17:44:13 +1030

    Removed last name and phone number contact field

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -43,17 +43,11 @@ export default function Contact() {
         </Typography> 
           <form>
             <Grid container spacing={1}>
-              <Grid xs={12} sm={6} item>
+              <Grid item xs={12}>
                 <TextField placeholder="Enter first name" label="First Name" variant="outlined" fullWidth required />
-              </Grid>
-              <Grid xs={12} sm={6} item>
-                <TextField placeholder="Enter last name" label="Last Name" variant="outlined" fullWidth required />
               </Grid>
               <Grid item xs={12}>
                 <TextField type="email" placeholder="Enter email" label="Email" variant="outlined" fullWidth required />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField type="number" placeholder="Enter phone number" label="Phone" variant="outlined" fullWidth required />
               </Grid>
               <Grid item xs={12}>
                 <TextField label="Message" multiline rows={4} placeholder="Type your message here" variant="outlined" fullWidth required />
