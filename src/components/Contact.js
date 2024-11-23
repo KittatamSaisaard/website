e3daf5593102ce7b5f5e931f5e7d5d3395ffdcbc
Kit <50354903+ksaisaard@users.noreply.github.com>
2024-11-23 17:24:58 +1030

    Updated Netlify forms test to add POST method

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -41,12 +41,15 @@ export default function Contact() {
               </Grid>
             </Grid>
           </form> */}
-          <form name="contact" netlify>
+          <form name="contact" method="POST" netlify>
             <p>
               <label>Name <input type="text" name="name" /></label>
             </p>
             <p>
               <label>Email <input type="email" name="email" /></label>
+            </p>  
+            <p>
+              <label>Message: <textarea name="message"></textarea></label>
             </p>
             <p>
               <button type="submit">Send</button>
