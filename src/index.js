74feb67110353deb8d3d1c0a6c946fac00434e03
Kit <50354903+ksaisaard@users.noreply.github.com>
2024-11-23 18:44:54 +1030

    Revert "Changed routing settings"
    This reverts commit b31a29c98355b40c16ce3c14b9a010bf686544ee.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-// import {
-//   BrowserRouter,
-//   Routes,
-//   Route
-// } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import './css/index.css';
 import ReactDOM from 'react-dom'
 import Default from './components/Default';
@@ -13,18 +13,17 @@ import Default from './components/Default';
 // import REC from './components/Retail-Earnings-Calculator';
 
 ReactDOM.render(
-  <Default/>,
-  // <React.StrictMode>
-  //   <BrowserRouter basename={process.env.PUBLIC_URL}>
-  //     <Routes>
-  //       <Route exact path="/" element={<Default/>}/>
-  //       {/* <Route exact path="/education" element={<Default page="education"/>}/>
-  //       <Route exact path="/experience" element={<Default page="experience"/>}/>
-  //       <Route exact path="/projects" element={<Default page="projects"/>}/>
-  //       <Route exact path="/about" element={<Default page="about"/>}/> */}
-  //       {/* <Route exact path="*" element={<NotFound/>}/> */}
-  //     </Routes>
-  //   </BrowserRouter>
-  // </React.StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route exact path="/" element={<Default/>}/>
+        {/* <Route exact path="/education" element={<Default page="education"/>}/>
+        <Route exact path="/experience" element={<Default page="experience"/>}/>
+        <Route exact path="/projects" element={<Default page="projects"/>}/>
+        <Route exact path="/about" element={<Default page="about"/>}/> */}
+        {/* <Route exact path="*" element={<NotFound/>}/> */}
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
   document.getElementById('root')
 );
