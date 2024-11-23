6b5b5ddd1c61ba9d3e77181e5630414334241732
Kit <50354903+ksaisaard@users.noreply.github.com>
2024-11-23 18:50:52 +1030

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 import './css/index.css';
 import ReactDOM from 'react-dom'
 import Default from './components/Default';
@@ -13,17 +13,31 @@ import Default from './components/Default';
 // import REC from './components/Retail-Earnings-Calculator';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route exact path="/" element={<Default/>}/>
-        {/* <Route exact path="/education" element={<Default page="education"/>}/>
-        <Route exact path="/experience" element={<Default page="experience"/>}/>
-        <Route exact path="/projects" element={<Default page="projects"/>}/>
-        <Route exact path="/about" element={<Default page="about"/>}/> */}
-        {/* <Route exact path="*" element={<NotFound/>}/> */}
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Default/>,
+<<<<<<< HEAD
+  // // <React.StrictMode>
+  // //   <BrowserRouter basename={process.env.PUBLIC_URL}>
+  // //     <Routes>
+  //       // <Route exact path="/" element={<Default/>}/>
+=======
+  // <React.StrictMode>
+  //   <BrowserRouter basename={process.env.PUBLIC_URL}>
+  //     <Routes>
+  //       <Route exact path="/" element={<Default/>}/>
+>>>>>>> parent of 74feb67 (Revert "Changed routing settings")
+  //       {/* <Route exact path="/education" element={<Default page="education"/>}/>
+  //       <Route exact path="/experience" element={<Default page="experience"/>}/>
+  //       <Route exact path="/projects" element={<Default page="projects"/>}/>
+  //       <Route exact path="/about" element={<Default page="about"/>}/> */}
+  //       {/* <Route exact path="*" element={<NotFound/>}/> */}
+<<<<<<< HEAD
+  //     {/* </Routes>
+  //   </BrowserRouter>
+  // </React.StrictMode>, */}
+=======
+  //     </Routes>
+  //   </BrowserRouter>
+  // </React.StrictMode>,
+>>>>>>> parent of 74feb67 (Revert "Changed routing settings")
   document.getElementById('root')
 );
