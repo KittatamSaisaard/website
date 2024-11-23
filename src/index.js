9504b1821ba3f17f53cfe36e196e8ec0142a6c3e
Kit <50354903+ksaisaard@users.noreply.github.com>
2024-11-23 17:32:48 +1030

    Updated routes

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import ReactDOM from 'react-dom'
 import Default from './components/Default';
 // import Education from './components/Education';
 // import About from './components/About';
-// import NotFound from './components/NotFound';
+import NotFound from './components/NotFound';
 // import REC from './components/Retail-Earnings-Calculator';
 
 ReactDOM.render(
@@ -17,11 +17,11 @@ ReactDOM.render(
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route exact path="/" element={<Default/>}/>
-        <Route exact path="/education" element={<Default page="education"/>}/>
+        {/* <Route exact path="/education" element={<Default page="education"/>}/>
         <Route exact path="/experience" element={<Default page="experience"/>}/>
         <Route exact path="/projects" element={<Default page="projects"/>}/>
-        <Route exact path="/about" element={<Default page="about"/>}/>
-        <Route exact path="*" element={<Default page="404"/>}/>
+        <Route exact path="/about" element={<Default page="about"/>}/> */}
+        {/* <Route exact path="*" element={<NotFound/>}/> */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
