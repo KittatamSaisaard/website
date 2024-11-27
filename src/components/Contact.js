c698f3aae194b4de092a47dabdd03705eb68d9fa
Kit <50354903+ksaisaard@users.noreply.github.com>
2024-11-27 17:44:00 +1030

    Changed sizing of contact form description and layout

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -24,30 +24,30 @@ export default function Contact() {
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams(formData).toString()
     })
-      .then(() => alert("Thank you for your submission. Your information has been recieved and I will try to get back to you as soon as possible!"))
+      .then(() => alert("Thank you for your submission. Your information has been recieved and I will try to get back to you as soon as possible!"), myForm.reset())
       .catch(error => alert(error));
   };
 
   return (
-    <Box component="section" id="contact" sx={{py: 15, textAlign: 'center'}}> 
+    <Box component="section" id="contact" sx={{my: 12, textAlign: 'center'}}> 
      <Typography variant="h1" gutterBottom>Contact Me</Typography>
-     <Typography variant="body1" color="textSecondary" component="p" align="center" gutterBottom sx={{ marginBottom: 2}}>
+     <Typography variant="h5" color="textSecondary" component="p" align="center" gutterBottom sx={{ marginBottom: 10, mx: {lg: 0, xs: 20}}}>
           Feel free to Contact me by submitting the form below and I will get back to you as soon as possible
         </Typography> 
-    <Grid>
-      <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}>
+    <Grid sx={{px: {lg: 45, xs: 10}}}>
+      <Card style={{padding: "0 5px 0 5px", margin: "0 auto" }}>
         <CardContent>
           <form name="contact" method="post" data-netlify="true" onSubmit={handleSubmit}>
           <input type="hidden" name="form-name" value="contact" />
             <Grid container spacing={2}>
               <Grid item xs={12}>
-                <TextField name="name" type="text" placeholder="Enter name" label="Name" variant="outlined" fullWidth required />
+                <TextField name="name" type="text" placeholder="Enter name" label="Name" variant="outlined" fullWidth required inputProps={{maxLength: 100}}/>
               </Grid>
               <Grid item xs={12}>
-                <TextField name="email" type="email" placeholder="Enter email" label="Email" variant="outlined" fullWidth required />
+                <TextField name="email" type="email" placeholder="Enter email" label="Email" variant="outlined" fullWidth required inputProps={{maxLength: 100}}/>
               </Grid>
               <Grid item xs={12}>
-                <TextField name="message" label="Message" multiline rows={4} placeholder="Type your message here" variant="outlined" fullWidth required />
+                <TextField name="message" label="Message" multiline rows={8} placeholder="Type your message here" variant="outlined" fullWidth required inputProps={{maxLength: 1000}}/>
               </Grid>
               <Grid item xs={12}>
                 <Button type="submit" variant="contained" color="primary" fullWidth>Submit</Button>
