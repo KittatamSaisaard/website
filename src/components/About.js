ba28d17991896fa8c8ee0cc08f5ae7d9a17b55f5
Kit <50354903+ksaisaard@users.noreply.github.com>
2024-12-04 10:18:58 +1030

    The skills box is now side by side when the website is viewed in a large view width

--- src/components/About.js
+++ src/components/About.js
@@ -21,58 +21,60 @@ export default function About() {
     />
   );
 
-  // const swift = '[![Swift](https://img.shields.io/badge/Swift-F54A2A?logo=swift&logoColor=white)](https://developer.apple.com/swift/)';
-  // const html = '[![HTML](https://img.shields.io/badge/HTML-%23E34F26.svg?logo=html5&logoColor=white)](https://developer.mozilla.org/en-US/docs/Web/HTML)'
-  // const css = '[![CSS](https://img.shields.io/badge/CSS-1572B6?logo=css3&logoColor=fff)](https://developer.mozilla.org/en-US/docs/Web/CSS)';
-  // const javascript = '[![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?logo=javascript&logoColor=000)](https://developer.mozilla.org/en-US/docs/Web/JavaScript)';
-  // const react = '[![React](https://img.shields.io/badge/React-%2320232a.svg?logo=react&logoColor=%2361DAFB)](https://react.dev/)';
-  // const sass = '[![Sass](https://img.shields.io/badge/Sass-C69?logo=sass&logoColor=fff)](https://sass-lang.com/)';
-  // const mui = '[![Material](https://img.shields.io/badge/Material%20UI-007FFF?style=for-the-badge&logo=mui&logoColor=white)](https://mui.com/)';
-  // const cSharp = '[![C#](https://custom-icon-badges.demolab.com/badge/C%23-%23239120.svg?logo=cshrp&logoColor=white)](https://learn.microsoft.com/en-us/dotnet/csharp/)';
-  // const unity = '[![Unity](https://img.shields.io/badge/Unity-%23000000.svg?logo=unity&logoColor=white)](https://unity.com/)';
+  const swift = '[![Swift](https://img.shields.io/badge/Swift-F54A2A?logo=swift&logoColor=white)](https://developer.apple.com/swift/)';
+  const html = '[![HTML](https://img.shields.io/badge/HTML-%23E34F26.svg?logo=html5&logoColor=white)](https://developer.mozilla.org/en-US/docs/Web/HTML)'
+  const css = '[![CSS](https://img.shields.io/badge/CSS-1572B6?logo=css3&logoColor=fff)](https://developer.mozilla.org/en-US/docs/Web/CSS)';
+  const javascript = '[![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?logo=javascript&logoColor=000)](https://developer.mozilla.org/en-US/docs/Web/JavaScript)';
+  const react = '[![React](https://img.shields.io/badge/React-%2320232a.svg?logo=react&logoColor=%2361DAFB)](https://react.dev/)';
+  const sass = '[![Sass](https://img.shields.io/badge/Sass-C69?logo=sass&logoColor=fff)](https://sass-lang.com/)';
+  const mui = '[![Material](https://img.shields.io/badge/Material%20UI-007FFF?style=for-the-badge&logo=mui&logoColor=white)](https://mui.com/)';
+  const cSharp = '[![C#](https://custom-icon-badges.demolab.com/badge/C%23-%23239120.svg?logo=cshrp&logoColor=white)](https://learn.microsoft.com/en-us/dotnet/csharp/)';
+  const unity = '[![Unity](https://img.shields.io/badge/Unity-%23000000.svg?logo=unity&logoColor=white)](https://unity.com/)';
 
-  // const skillsList = [html, css, javascript, react, sass, mui, cSharp, unity, swift];
+  const skillsList = [html, css, javascript, react, sass, mui, cSharp, unity, swift];
 
   return (
     <Box component="section" id="about" sx={{textAlign: 'center', backgroundColor: '#f2f2f2', pt: 15, px: {lg: 20, xs: 0}, pb: 6}}>
       <Typography sx={{ typography: {xs: 'h2', lg: 'h1'}}} gutterBottom>About Me</Typography>
-      <Box component="div" className='about' sx={{backgroundColor: '#f7f7f7', m: {lg: 0, xs: 2}}}>
-          <Typography variant="body2" id="bio" sx={{ color: 'text.secondary', fontSize: {xs: 15, md: 20}, textAlign: 'center', m: 5}}>
-          As a tech enthusiast passionate about solving complex problems, I thrive at the intersection of curiosity and innovation. With experience in software development and emerging technologies, I focus on creating user-centric applications that enhance efficiency and push boundaries. I’m committed to continuous learning, fostering collaboration, and building solutions that make a meaningful impact.
-          <br></br><br></br>
-          When I’m not coding or brainstorming ideas, you’ll find me solving Rubik’s cubes, exploring new trails, or diving into the latest tech trends. Let’s connect, collaborate and shape the future of technology together!
-          </Typography>
+      <Box sx={{display: 'flex', flexDirection: {xs: 'column', lg: 'row'}, alignItems: 'stretch', justifyContent: 'center', gap: 5}}>
+        <Box component="div" className='about' sx={{backgroundColor: '#f7f7f7', m: {lg: 0, xs: 2}, flex: 1}}>
+            <Typography variant="body2" id="bio" sx={{ color: 'text.secondary', fontSize: {xs: 15, md: 20}, textAlign: 'center', m: 5}}>
+            As a tech enthusiast passionate about solving complex problems, I thrive at the intersection of curiosity and innovation. With experience in software development and emerging technologies, I focus on creating user-centric applications that enhance efficiency and push boundaries. I’m committed to continuous learning, fostering collaboration, and building solutions that make a meaningful impact.
+            <br></br><br></br>
+            When I’m not coding or brainstorming ideas, you’ll find me solving Rubik’s cubes, exploring new trails, or diving into the latest tech trends. Let’s connect, collaborate and shape the future of technology together!
+            </Typography>
+        </Box>
+        {/* <Box component="div" sx={{backgroundColor: '#f7f7f7', m: {lg: 0, xs: 2}, flex: 1}}>
+            <Typography variant="h2" sx={{color: '#589ebf', textAlign: 'center', m: 5}}>
+              My Skills:
+            </Typography>
+            <Box
+                  sx={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    gap: '7px',
+                    margin: 0,
+                    p: 1,
+                  }}
+                >
+                  {skillsList.map((tech, index) => (
+                    <Markdown key={index} components={{ a: LinkRenderer, img: ImageRenderer, 
+                      p: ({ node, ...props }) => (
+                        <Typography component="span" sx={{ m: 0, p: 0 }}>
+                          {props.children}
+                        </Typography>
+                      )
+                    }}>
+                      {tech}
+                    </Markdown>
+                  ))}
+            </Box>
+        </Box> */}
       </Box>
-      {/* <Box component="div" sx={{backgroundColor: '#f7f7f7', m: {lg: 0, xs: 2}}}>
-          <Typography variant="h2" sx={{color: '#589ebf', textAlign: 'center', m: 5}}>
-            My Skills:
-          </Typography>
-          <Box
-                sx={{
-                  display: 'flex',
-                  flexWrap: 'wrap',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  gap: '7px',
-                  margin: '0 auto',
-                  p: 1
-                }}
-              >
-                {skillsList.map((tech, index) => (
-                  <Markdown key={index} components={{ a: LinkRenderer, img: ImageRenderer, 
-                    p: ({ node, ...props }) => (
-                      <Typography component="span" sx={{ m: 0, p: 0 }}>
-                        {props.children}
-                      </Typography>
-                    )
-                  }}>
-                    {tech}
-                  </Markdown>
-                ))}
-          </Box>
-      </Box>
-      <br></br><br></br> */}
-      <Box sx={{backgroundColor: '#f7f7f7', p: {xs: 1, xl: 2}, pb: {xs: 5}, pt: {xs: 1}, textAlign: 'center', display: 'flex', flexDirection: {xs: 'column', xl: 'row'}, alignItems: 'center', mx: {xs: 2, xl: 0}}}>
+      <br></br>
+      <Box sx={{backgroundColor: '#f7f7f7', p: {xs: 1, xl: 2}, pb: {xs: 5}, pt: {xs: 1}, textAlign: 'center', display: 'flex', flexDirection: {xs: 'column', xl: 'row'}, alignItems: 'center', m: {lg: 0, xs: 2}}}>
             <Typography color="primary" sx={{typography: {xs: 'h5', sm: 'h4', md: 'h3'}, m: {xs: 2, xl: '65px'}}}>Connect with me on LinkedIn</Typography>
             <Box component="img" border="0" src={linkedIn_logo} alt="LinkedIn Logo" onClick = {() => window.open('https://www.linkedin.com/in/kittatam-saisaard/', '_blank')} sx={{cursor: 'pointer'}}/>
       </Box>
